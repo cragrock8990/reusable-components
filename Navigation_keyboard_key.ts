ca451faee8224b18ebfcf7f7ecf3b9238913cc98
cragrock8990 <37631777+cragrock8990@users.noreply.github.com>
2021-10-19 16:38:55 +0100

    Create Navigation_keyboard_key.ts

---
+++ Navigation_keyboard_key.ts
@@ -0,0 +1,49 @@
+// keycode 8
+export const BACKSPACE = 'Backspace';
+
+// keycode 9
+export const TAB = 'Tab';
+
+// keycode 13
+export const ENTER = 'Enter';
+export const NUMPAD_ENTER = 'NumpadEnter';
+
+// keycode 27
+export const ESCAPE = 'Escape';
+
+// keycode 33
+export const PAGE_UP = 'PageUp';
+
+// keycode 34
+export const PAGE_DOWN = 'PageDown';
+
+// keycode 35
+export const END = 'End';
+
+// keycode 36
+export const HOME = 'Home';
+
+// keycode 37
+export const LEFT_ARROW = 'ArrowLeft';
+
+// keycode 38
+export const UP_ARROW = 'ArrowUp';
+
+// keycode 39
+export const RIGHT_ARROW = 'ArrowRight';
+
+// keycode 40
+export const DOWN_ARROW = 'ArrowDown';
+
+// keycode 43
+export const EXECUTE = "execute";
+
+// keycode 44
+export const PRINT_SCREEN = "PrintScreen";
+
+// keycode 46
+export const DELETE = 'Delete';
+
+// keycode 91
+export const OS_LEFT = 'OSLeft';
+export const SCROLL_LOCK = 'ScrollLock';
